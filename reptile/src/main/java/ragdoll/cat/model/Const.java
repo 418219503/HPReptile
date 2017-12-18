b649ffc303fc375b27c0232fc5ef0c4fd10e341d
418219503 <418219503@qq.com>
2017-12-18 20:35:13 +0800

    Add files via upload

--- reptile/src/main/java/ragdoll/cat/model/Const.java
+++ reptile/src/main/java/ragdoll/cat/model/Const.java
@@ -7,16 +7,33 @@ public class Const {
 
 	// 城市名
 	/*
-	 * 安居客没有以下城市房价信息
-	 * "xianggang:香港","aomen:澳门",
+	 * 安居客没有以下城市房价信息 "xianggang:香港","aomen:澳门",
+	 * 
+	 * 
+	 * "chengdu:成都", "beijing:北京", "tianjin:天津", "chongqing:重庆", "shanghai:上海",
+	 * "lasa:拉萨", "nanning:南宁", "yinchuan:银川", "wulumuqi:乌鲁木齐", "huhehaote:呼和浩特",
+	 * "sjz:石家庄", "ty:太原", "sy:沈阳", "cc:长春", "heb:哈尔滨", "nanjing:南京", "hangzhou:杭州",
+	 * "hf:合肥", "fz:福州", "nc:南昌", "jinan:济南", "zhengzhou:郑州", "wuhan:武汉", "cs:长沙",
+	 * "guangzhou:广州", "haikou:海口", "gy:贵阳", "km:昆明", "xa:西安", "lanzhou:兰州",
+	 * "xining:西宁","dalian:大连", "jilin:吉林", "wuxi:无锡", "ningbo:宁波", "wenzhou:温州",
+	 * "suzhou:苏州", "xiamen:厦门", "qingdao:青岛", "luoyang:洛阳", "shenzhen:深圳",
+	 * "zhuhai:珠海", "guilin:桂林", "sanya:三亚", "mianyang:绵阳", "hanzhong:汉中",
+	 * "huangshi:黄石", "daqing:大庆"
 	 */
-	public final static String[] CITY_NAME = { "chengdu:成都", "beijing:北京", "tianjin:天津", "chongqing:重庆", "shanghai:上海", "lasa:拉萨", "nanning:南宁", "yinchuan:银川",
-			"wulumuqi:乌鲁木齐", "huhehaote:呼和浩特",  "sjz:石家庄", "ty:太原", "sy:沈阳", "cc:长春", "heb:哈尔滨", "nanjing:南京",
-			"hangzhou:杭州", "hf:合肥", "fz:福州", "nc:南昌", "jinan:济南", "zhengzhou:郑州", "wuhan:武汉", "cs:长沙", "guangzhou:广州", "haikou:海口", "gy:贵阳", "km:昆明",
-			"xa:西安", "lanzhou:兰州", "xining:西宁"};
-	
+	public final static String[] CITY_NAME = {"chengdu:成都", "beijing:北京", "tianjin:天津", "chongqing:重庆", "shanghai:上海",
+			  "lasa:拉萨", "nanning:南宁", "yinchuan:银川", "wulumuqi:乌鲁木齐", "huhehaote:呼和浩特",
+			  "sjz:石家庄", "ty:太原", "sy:沈阳", "cc:长春", "heb:哈尔滨", "nanjing:南京", "hangzhou:杭州",
+			  "hf:合肥", "fz:福州", "nc:南昌", "jinan:济南", "zhengzhou:郑州", "wuhan:武汉", "cs:长沙",
+			  "guangzhou:广州", "haikou:海口", "gy:贵阳", "km:昆明", "xa:西安", "lanzhou:兰州",
+			  "xining:西宁",
+			  "dalian:大连", "jilin:吉林", "wuxi:无锡", "nb:宁波", "wenzhou:温州",
+			  "suzhou:苏州", "xm:厦门", "qd:青岛", "luoyang:洛阳", "shenzhen:深圳",
+			  "zh:珠海", "guilin:桂林", "sanya:三亚", "mianyang:绵阳", "hanzhong:汉中",
+			  "huangshi:黄石", "daqing:大庆"};
+
 	/*
 	 * "2017","2016","2015","2014","2013","2012","2011","2010","2009","2008"
 	 */
-	public final static String[] YEAR = {"2017","2016","2015","2014","2013","2012","2011","2010","2009","2008"};
+	public final static String[] YEAR = { "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009",
+			"2008" };
 }
